93c2e0cc4d7178718cad2bacf00ba620e54af1ae
Donovan Dall <donovandall@icloud.com>
2020-12-08 20:52:33 +0000

    move some stuff

---
+++ src/cli.rs
@@ -0,0 +1,9 @@
+    /// Take cli data
+    ///
+    /// Location or raw file
+    /// Pass a script(rust or bash script??, we could make it from a list of commands??)
+    /// into a folder with parameters as some format(YAML, JSON?)
+    /// param whether its sequential, parallel
+    /// whether it requires data posting!
+    /// file location
+    /// workers, how many? (Kubernetes could create them, should be quick to spawn)
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
 mod cli;
 
 fn main() {
-    /// Take cli data
-    ///
-    /// Pass a script(rust or bash script??, we could make it from a list of commands??)
-    /// into a folder with parameters as some format(YAML, JSON?)
-    /// param whether its sequential, parallel
-    /// whether it requires data posting
-    /// file location
-    /// workers, how many? (Kubernetes could create them, should be quick to spawn)
-
     /// Receive events
     /// How?
     /// Watch FS(easy over NFS, S3 and so on, lightweight, no services stood up) - notify
