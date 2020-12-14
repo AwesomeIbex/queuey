c5f970880db6d99cba88388e1bb299629b735127
Donovan Dall <donovandall@icloud.com>
2020-12-14 13:38:57 +0000

    logs

--- src/main.rs
+++ src/main.rs
@@ -25,37 +25,34 @@ fn main() {
 
     let cli_opts = cli::get_opts_args();
     let job_id = rand::thread_rng().sample_iter(&Alphanumeric).take(20).collect::<String>();
-    // Receive events
-    // How?
-    // Watch FS(easy over NFS, S3 and so on, lightweight, no services stood up) - notify
-    // Wait over REST
-    // MQ(heavyweight??, requires client side implementation, could be a module) - zmq
 
-    // Watcher related stuff
+    // could get workers here
+
     let (watcher_tx, watcher_rx) = channel();
     let mut watcher = watcher(watcher_tx, Duration::from_millis(300)).unwrap(); //TODO test delay
     fs::create_dir_all(&cli_opts.jobs_path);
     watcher.watch(&cli_opts.jobs_path, RecursiveMode::Recursive).unwrap();
 
-    // how does one dispatch a job to a worker, do we have an assumed directory which each worker will
-    // create themselves a folder, when a file is dropped into their folder they just run the script in the location and then
-    // append the job file with the worker prefix and whether it completed etc?
     dispatcher::dispatch(&job_id, &cli_opts, DispatchType::FILE); //TODO might param this but for now its file
     loop {
         match watcher_rx.recv() {
             Ok(event) => {
                 match event {
-                    DebouncedEvent::NoticeWrite(_) => {} // Might need to close file access
-                    DebouncedEvent::NoticeRemove(_) => {} // Same as above
-                    DebouncedEvent::Create(_) => {} // Log depending on prefix of the name OR could use permissions
+                    DebouncedEvent::Create(path) => {
+                        log::trace!("A file was created at path: {:?}", path)
+                    } // Log depending on prefix of the name OR could use permissions
                     DebouncedEvent::Write(_) => {} // Log depending on prefix of the name
-                    DebouncedEvent::Chmod(_) => {} // Could be smart depending on permissions maybe
-                    DebouncedEvent::Remove(_) => {} // Job is finished or worker deregistered store that elsewhere
+                    DebouncedEvent::Remove(path) => {
+                        log::trace!("A worker deregistered, path: {:?}", path)
+                    }
                     DebouncedEvent::Rename(_, _) => {} // Job is finished, store that elsewhere
-                    DebouncedEvent::Rescan => {} // Log
-                    DebouncedEvent::Error(_, _) => {} // Log
+                    DebouncedEvent::Error(err, path) => {
+                        log::error!("There was an error at path {:?}, {}", path, err)
+                    } // Log
+                    _ => {
+                        log::trace!("An event was triggered {:?}", event)
+                    }
                 }
-                println!("Recieved event {:?}", event);
             },
             Err(e) => dispatcher::dispatch_error(e),
         }
