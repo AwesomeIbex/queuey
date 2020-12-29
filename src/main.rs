210196732359f0e8523348ecf0048b724f44b03a
Donovan Dall <donovandall@icloud.com>
2020-12-29 11:17:23 +0000

    basic log watching

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ use rand::Rng;
 
 use crate::dispatcher::DispatchType;
 use crate::cli::Platform;
+use anyhow::Context;
+use std::path::PathBuf;
+use logwatcher::{LogWatcher, LogWatcherAction};
+use std::sync::Arc;
+use rayon::prelude::*;
 
 mod cli;
 mod dispatcher;
@@ -44,12 +49,29 @@ async fn main() {
     watcher.watch(&cli_opts.jobs_path, RecursiveMode::Recursive).unwrap();
 
     dispatcher::dispatch(&job_id, &cli_opts, DispatchType::FILE); //TODO might param this but for now its file
+
+    let (logs_tx, logs_rx) = std::sync::mpsc::channel();
+    std::thread::spawn(|| {
+
+        logs_rx.into_iter().par_bridge().for_each(|(job, path)| {
+            let mut log_watcher = LogWatcher::register(path).unwrap();
+
+            log_watcher.watch(&mut move |line: String| {
+                log::info!("[{}] {}", job, line);
+                LogWatcherAction::None
+            });
+        });
+    });
+
     loop {
         match watcher_rx.recv() {
             Ok(event) => {
                 match event {
                     DebouncedEvent::Create(path) => {
-                        log::trace!("A file was created at path: {:?}", path)
+                        log::trace!("A file was created at path: {:?}", path);
+                        if path.to_str().context("Couldnt turn the path to a string").unwrap().contains("logs") {
+                            logs_tx.send((job_id.clone(), path));
+                        }
                     } // Log depending on prefix of the name OR could use permissions
                     DebouncedEvent::Write(_) => {} // Log depending on prefix of the name
                     DebouncedEvent::Remove(path) => {
@@ -59,6 +81,7 @@ async fn main() {
                     DebouncedEvent::Error(err, path) => {
                         log::error!("There was an error at path {:?}, {}", path, err)
                     } // Log
+                    DebouncedEvent::NoticeWrite(path) => {}
                     _ => {
                         log::trace!("An event was triggered {:?}", event)
                     }
